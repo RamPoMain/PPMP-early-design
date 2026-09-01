cc96048c1a18cc0ac31719d197cb7180c2fb61a7
RamPo <86035339+RamPoMain@users.noreply.github.com>
2026-09-01 13:43:55 +0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,31 @@
-// GO FORWARD FUNCTIONS
-  function showNextStep() { // To Step 2
+function showNextStep() {
     document.getElementById('step-1').classList.add('hidden');
     document.getElementById('step-2').classList.remove('hidden');
     document.getElementById('page-title').innerText = "PROJECTED TIMELINE";
     document.getElementById('progress-fill').style.width = "45%";
     window.scrollTo(0,0);
-  }
+}
 
-  function showStep3() { // To Step 3
+function showStep3() {
     document.getElementById('step-2').classList.add('hidden');
     document.getElementById('step-3').classList.remove('hidden');
     document.getElementById('page-title').innerText = "FUNDING DETAILS";
     document.getElementById('progress-fill').style.width = "85%";
     window.scrollTo(0,0);
-  }
+}
 
-  // NEW GO BACK FUNCTIONS
-  function showStep1() { // Back to Step 1
+function showStep1() {
     document.getElementById('step-2').classList.add('hidden');
     document.getElementById('step-1').classList.remove('hidden');
     document.getElementById('page-title').innerText = "PROCUREMENT PROJECT DETAILS";
     document.getElementById('progress-fill').style.width = "15%";
     window.scrollTo(0,0);
-  }
+}
 
-  function showStep2() { // Back to Step 2
+function showStep2() {
     document.getElementById('step-3').classList.add('hidden');
     document.getElementById('step-2').classList.remove('hidden');
     document.getElementById('page-title').innerText = "PROJECTED TIMELINE";
     document.getElementById('progress-fill').style.width = "45%";
     window.scrollTo(0,0);
-  }
+}
